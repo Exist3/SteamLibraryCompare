f4dd57bbd2bdede747b4f5ba85dd2fdf44b6ec06
Samuel Parchert <parchertsam@gmail.com>
2019-02-26 18:17:50 +1000

    Added comments

--- SteamLibraryCompare.py
+++ SteamLibraryCompare.py
@@ -1,8 +1,14 @@
+# Import Files
 import requests
 import json
 
 
 def get_account_info(steam_ids):
+    """
+    Returns the results of a steam api get request containing a summary of one or more steam users.
+    :param steam_ids: A list containing the steam ids of all steam accounts you want info for.
+    :return: A list containing information about one or more Steam profiles. None if request fails
+    """
 
     api_url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={0}&steamids={1}"\
         .format(api_token, steam_ids)
@@ -16,6 +22,11 @@ def get_account_info(steam_ids):
 
 
 def get_friends(steam_id):
+    """
+    Returns the results of a steam api get request containing information about a users friend list.
+    :param steam_id: A string containing the steam ID of the user to check.
+    :return: The response of the aforementioned get request. None if request fails
+    """
 
     api_url = "http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={0}&steamid={1}&relationship=friend"\
         .format(api_token, steam_id)
@@ -29,6 +40,11 @@ def get_friends(steam_id):
 
 
 def get_steam_name(steam_id):
+    """
+    Returns a user's steam display name given their steam ID
+    :param steam_id: The ID of the steam user as a string
+    :return: A string containing rhe user's steam display name. None if request fails.
+    """
 
     data = get_account_info(steam_id)
     if data:
@@ -38,6 +54,11 @@ def get_steam_name(steam_id):
 
 
 def get_friend_ids(steam_id):
+    """
+    Returns a list of steam IDs corresponding to a user's friend list.
+    :param steam_id: The user who's friend list should be checked.
+    :return: A list of strings containing the steam ids of the user's friends. None if request fails.
+    """
 
     output = []
     data = get_friends(steam_id)
@@ -50,6 +71,11 @@ def get_friend_ids(steam_id):
 
 
 def get_owned_games(steam_id):
+    """
+    Returns the results of a steam api get request containing information about a user's owned games.
+    :param steam_id: The steam user to check.
+    :return: The results of the aforementioned get request. None if request fails.
+    """
 
     api_url = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={0}&steamid={1}&include_appinfo=1" \
         .format(api_token, steam_id)
@@ -63,6 +89,11 @@ def get_owned_games(steam_id):
 
 
 def get_game_list(steam_id):
+    """
+    Returns a list of owned games given a user's steam ID.
+    :param steam_id: A string containing the user's steam ID.
+    :return: A list containing the names of all of the user's owned games.
+    """
 
     data = get_owned_games(steam_id)
     if data:
